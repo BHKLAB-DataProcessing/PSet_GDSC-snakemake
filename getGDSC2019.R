ae11a8bc03d8db9612655ab795908735d408958d
imanthonny <34114220+imanthonny@users.noreply.github.com>
2020-04-06 21:35:53 -0400

    Update getGDSC2019.R

--- getGDSC2019.R
+++ getGDSC2019.R
@@ -490,6 +490,7 @@ rownames(drug_all) <- drug_all[ , "unique.drugid"]
 drug_all <- drug_all[rownames(drug.info),]
 drug.info[,c("smiles","inchikey","cid","FDA")] <- drug_all[,c("smiles","inchikey","cid","FDA")]
 
+curationDrug <- curationDrug[rownames(drug.info),]
 
 message("Making PSet")
 
