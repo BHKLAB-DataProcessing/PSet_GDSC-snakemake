6a6c876bdb6ff691ca3e7375e608abb8c4821347
imanthonny <34114220+imanthonny@users.noreply.github.com>
2020-04-09 15:25:57 -0400

    Update getGDSC2019.R

--- getGDSC2019.R
+++ getGDSC2019.R
@@ -598,30 +598,31 @@ z <- c(z,c(
 
 		 
 #add cellosaurus disease type to cell-info
-colnames(cell.info)[which(names(cell.info) == "unique.cellid")] <- "cellid"
-disease <- cell_all$Cellosaurus.Disease.Type[match(cell.info$cellid, cell_all$unique.cellid)]
+
+disease <- cell_all$Cellosaurus.Disease.Type[match(cell.info$unique.cellid, cell_all$unique.cellid)]
 cell.info$Cellosaurus.Disease.Type <- disease		 
 
 #add cellosaurus assession to cell-info
-assession <- cell_all$Cellosaurus.Accession.id[match(cell.info$cellid, cell_all$unique.cellid)]
+assession <- cell_all$Cellosaurus.Accession.id[match(cell.info$unique.cellid, cell_all$unique.cellid)]
 cell.info$Cellosaurus.Accession.id <- assession
 		 
 #add pharmacodb id to cell-info
-pdb <- cell_all$PharmacoDB.id[match(cell.info$cellid, cell_all$unique.cellid)]
+pdb <- cell_all$PharmacoDB.id[match(cell.info$unique.cellid, cell_all$unique.cellid)]
 cell.info$PharmacoDB.id <- pdb
 
 #add study tissue id to cell_info
-study_tissue <- cell_all$unique.tissueid.fromstudies[match(cell.info$cellid, cell_all$unique.cellid)]
+study_tissue <- cell_all$unique.tissueid.fromstudies[match(cell.info$unique.cellid, cell_all$unique.cellid)]
 cell.info$unique.tissueid.fromstudies <- study_tissue
 		 
 #add study cell-line type to cell_info
-cell_type <- cell_all$CellLine.Type[match(cell.info$cellid, cell_all$unique.cellid)]
+cell_type <- cell_all$CellLine.Type[match(cell.info$unique.cellid, cell_all$unique.cellid)]
 cell.info$CellLine.Type <- cell_type
 		 
 #add metastatic info to cell_info		 
-metastatic <- cell_all$Metastatic[match(cell.info$cellid, cell_all$unique.cellid)]
+metastatic <- cell_all$Metastatic[match(cell.info$unique.cellid, cell_all$unique.cellid)]
 cell.info$Metastatic <- metastatic		 
-		 
+cell.info <- cell.info[rownames(curationCell),]
+
 standardize <- standardizeRawDataConcRange(sens.info = sens.info, sens.raw = sens.raw)
 
 GDSC <- PharmacoSet(molecularProfiles=z,
